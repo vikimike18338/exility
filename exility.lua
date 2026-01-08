f06252166c9b1f954225ff27c9201a93ef9176cd
vikimike18338 <vikimike18338@gmail.com>
2026-01-08 22:12:20 +0100

    Update exility.lua

--- exility.lua
+++ exility.lua
@@ -1,12 +1,1 @@
-local v0="https://prisoners-sail-released-actress.trycloudflare.com";local v1=v0   .. "/script" ;local function v2(v4) local v5=0;local v6;local v7;while true do if (v5==(1 + 0)) then return nil;end if (v5==(0 + 0)) then v6,v7=pcall(function() return game:HttpGet(v4);end);if (v6 and v7 and (v7~="")) then return v7;end v5=1;end end end local v3=v2(v1);if  not v3 then local v8=1270 -(226 + 1044) ;local v9;while true do if (v8==(0 -0)) then v9=0 -0 ;while true do if (v9==0) then local v10=117 -(32 + 85) ;while true do if ((0 + 0)==v10) then warn("[Exility] Failed to load loader from /script");return;end end end end break;end end end loadstring(v3)();
--- ⚠️ WARNING: integrity protected!
---[[
- .____                  ________ ___.    _____                           __                
- |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
- |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
- |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
- |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
-         \/          \/         \/    \/                \/     \/     \/                   
-          \_Welcome to LuaObfuscator.com   (Alpha 0.10.9) ~  Much Love, Ferib 
-
-]]--
+loadstring(game:HttpGet(('https://gist.githubusercontent.com/vikimike18338/bd5728c18555fc5878b163aa01c066bb/raw/c552fa248a9fa4da4b91f930b3cf088de21554ff/SBNAoidbasod1249hAJSK'),true))()
