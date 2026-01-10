a637a538ceab9eb90d001ab2f7a41c5f176c3d91
vikimike18338 <vikimike18338@gmail.com>
2026-01-10 13:30:08 +0100

    Update exility.lua

--- exility.lua
+++ exility.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet(('https://gist.githubusercontent.com/vikimike18338/bd5728c18555fc5878b163aa01c066bb/raw/c552fa248a9fa4da4b91f930b3cf088de21554ff/SBNAoidbasod1249hAJSK'),true))()
+loadstring(game:HttpGet(('https://gist.githubusercontent.com/vikimike18338/bd5728c18555fc5878b163aa01c066bb/raw/5d9f4e574165b7da35fbff65df88607a34417f07/SBNAoidbasod1249hAJSK'),true))()
